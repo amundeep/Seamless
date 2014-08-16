b6e251722bac1589679ae89ecabb988462e41887
hyang97 <Henry@Henrys-MacBook-Air.local>
2014-08-16 05:02:39 -0700

    front/back shirt layout
    added overall layout, fragment 1 and 2, draw fragment 1 and 2 with
    canvas inside

--- gen/me/amundeep/seamless/R.java
+++ gen/me/amundeep/seamless/R.java
@@ -13,11 +13,21 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int content=0x7f060001;
+        public static final int frontBackTitle=0x7f060000;
+        public static final int surface1=0x7f060002;
+        public static final int surface2=0x7f060003;
+    }
     public static final class layout {
-        public static final int activity_splash=0x7f030000;
+        public static final int activity_shirtlayout=0x7f030000;
+        public static final int activity_splash=0x7f030001;
+        public static final int fragment_1=0x7f030002;
+        public static final int fragment_2=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int front_back_title=0x7f040001;
     }
     public static final class style {
         /** 
